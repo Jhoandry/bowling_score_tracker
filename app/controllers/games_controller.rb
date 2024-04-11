d50963268742eb71732adbd7665723eeab73b560
Jhoandry Sequera <jsequera826@gmail.com>
2024-04-11 21:23:07 +0200

    BG-003: start game controller and specs related

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,48 @@
 # In charge of new Game creations and give the score
 class GamesController < ApplicationController
   def create
-    render status: :ok, json: { result: 'ok' }
+    if start_game
+      render json: { game: serialize_game }, status: :ok
+    else
+      render json: { error: 'Failed to start the game' }, status: :unprocessable_entity
+    end
   end
 
   private
 
+  def start_game
+    return false if player_names.empty?
+
+    @players = create_players
+    @game = create_game
+    @players.present? && @game.present?
+  end
+
+  def create_players
+    player_names.map { |player_name| Player.create(name: player_name) }
+  end
+
+  def create_game
+    Game.create({ location: }.compact)
+  end
+
+  def location
+    permitted_params[:location]
+  end
+
+  def player_names
+    params.required(:players)
+  end
+
   def permitted_params
-    params.permit(:location, :players)
+    params.permit(:location, players: [])
+  end
+
+  def serialize_game
+    {
+      id: @game.id,
+      location: @game.location,
+      players: @players.map { |player| { id: player.id, name: player.name } }
+    }
   end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,15 +2,39 @@ RSpec.describe GamesController do
   describe '#create' do
     subject(:create) { post :create, format: :json, params: body_request }
 
-    let(:body_request) do
-      {
-        location: 'Freeletics lane',
-        players: ['player 1', 'player 2', 'player 3']
-      }
+    context 'with all required data' do
+      let(:body_request) do
+        {
+          location: 'Freeletics lane',
+          players: ['player 1', 'player 2', 'player 3']
+        }
+      end
+
+      let(:games) { Game.count }
+      let(:players) { Player.count }
+
+      it { is_expected.to have_http_status :ok }
+
+      it 'creates a new game' do
+        expect { create }.to change(Game, :count).by(1)
+      end
+
+      it 'creates new players' do
+        expect { create }.to change(Player, :count).by(3)
+      end
     end
 
-    it do
-      expect(subject).to have_http_status :ok
+    context 'without all required data start a new game' do
+      let(:body_request) do
+        {
+          players: []
+        }
+      end
+
+      it do
+        expect { create }.to raise_error(ActionController::ParameterMissing,
+                                         'param is missing or the value is empty: players')
+      end
     end
   end
 end
