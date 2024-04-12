bd7e39db68771a24ecdd97222674afae3eca1912
Jhoandry Sequera <jsequera826@gmail.com>
2024-04-12 12:58:20 +0200

    BG-0003: default turn score on 0 and move in a method build turn data by player

--- app/serializer/game_serializer.rb
+++ app/serializer/game_serializer.rb
@@ -11,13 +11,21 @@ class GameSerializer
       players: @game.players.map do |player|
         { id: player.id,
           name: player.name,
-          turns: player.turns.map { |turn| { number: turn.turn_number, score: turn.score, status: turn.status } },
+          turns: player_turs(player),
           total_score: total_score(player) }
       end
     }
   end
 
   private
+
+  def player_turs(player)
+    player.turns.map do |turn|
+      { number: turn.turn_number,
+        score: turn.score || 0,
+        status: turn.status }
+    end
+  end
 
   def total_score(player)
     player.turns.pluck(:score).compact.sum
